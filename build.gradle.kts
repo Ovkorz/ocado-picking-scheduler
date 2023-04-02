39915904ddddd8c3bbfac71d9954f92a74fc0512
simon <pupkaszymon@gmail.com>
2023-04-02 23:51:31 +0200

    add build configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,11 +14,17 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     implementation(platform("com.fasterxml.jackson:jackson-bom:2.13.4.1"))
-//    implementation("com.fasterxml.jackson.core:jackson-databind")
-//    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.4")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.5")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+
+    manifest {
+        attributes["Main-Class"] = "scheduler.Main"
+    }
+    from(configurations.runtimeClasspath.get().map({ if (it.isDirectory) it else zipTree(it) }))
 }
