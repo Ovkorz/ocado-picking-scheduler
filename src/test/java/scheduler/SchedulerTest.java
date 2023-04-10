fbe6e5936fdf21421b44975f8861a56c6871a549
simon <pupkaszymon@gmail.com>
2023-04-10 22:52:40 +0200

    add testScheduleTasksLogicBomb unit test add parametrized test for amount criteria scheduling

--- src/test/java/scheduler/SchedulerTest.java
+++ src/test/java/scheduler/SchedulerTest.java
@@ -3,47 +3,87 @@ package scheduler;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import testutil.TestCaseLoader;
+import testutil.TestData;
 import testutil.TestingTask;
 
 import java.io.IOException;
 
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.stream.Stream;
 
 class SchedulerTest {
 
     private static final String TEST_CASES_DIR = "src/test/resources";
-    private static TestCaseLoader loader;
 
-    @BeforeAll
-    public static void setUp() {
-        loader = new TestCaseLoader();
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    void testScheduleTasksAmountCriteria(TestData testData) {
+        Scheduler scheduler = testData.scheduler;
+        List<List<TestingTask>> expectedSchedules = testData.expectedSchedules;
+
+        List<Task> calculatedSchedule = scheduler.scheduleTasks();
+
+        boolean anyMatch = expectedSchedules.stream()
+                .anyMatch(schedule -> TestingTask.compareList(schedule, calculatedSchedule));
+        Assertions.assertTrue(anyMatch);
     }
+
+    private static Stream<Arguments> provideTestData() {
+        return Stream.of(
+                TestData.loadTestData(TEST_CASES_DIR + "/complete-by"),
+                TestData.loadTestData(TEST_CASES_DIR + "/isf-end-time"),
+                TestData.loadTestData(TEST_CASES_DIR + "/optimize-order-count"),
+//                TestData.loadTestData(TEST_CASES_DIR + "/logic-bomb"),
+                TestData.loadTestData(TEST_CASES_DIR + "/any-order-length-is-ok")
+        ).map(Arguments::of);
+    }
+
     @Test
-    void testScheduleTasksCompleteBy() throws IOException {
-        String base_path = TEST_CASES_DIR + "/complete-by";
-        String expected_schedules_path = base_path + "/output.txt";
-        List<List<TestingTask>> expected_schedules = TestCaseLoader.loadSchedules(expected_schedules_path);
+    void testScheduleTasksLogicBomb(){
+        String base_path = TEST_CASES_DIR + "/logic-bomb";
 
         JsonFileLoader loader = new JsonFileLoader();
 
-        String orders_path = base_path + "/orders.json";
-        List<Order> orders = loader.loadOrders(Paths.get(orders_path));
+//        List<Order> orders;
+        Store store = null;
+        List<Order> orders = null;
 
-        String store_path = base_path + "/store.json";
-        Store store = loader.loadStore(Paths.get(store_path));
+        try {
+            String orders_path = base_path + "/orders.json";
+            orders = loader.loadOrders(Paths.get(orders_path));
 
+            String store_path = base_path + "/store.json";
+            store = loader.loadStore(Paths.get(store_path));
+        }
+        catch (IOException er){
+            System.out.println("Error: " + er.getMessage());
+        }
+
+//        TestData testData =  TestData.loadTestData(TEST_CASES_DIR + "/logic-bomb");
+
+        assert store != null;
+        assert orders != null;
         Scheduler scheduler = new Scheduler(
                 store.getPickers(),
                 store.getPickingStartTime(),
                 store.getPickingEndTime(),
                 orders
         );
-        List<Task> calculated_schedule = scheduler.scheduleTasks();
 
-        boolean any_match = expected_schedules.stream()
-                .anyMatch(schedule -> TestingTask.compareList(schedule, calculated_schedule));
-        Assertions.assertTrue(any_match);
+        // Create thread that runs the scheduleTasks method and waits for it to complete
+        Thread scheduleThread = new Thread(scheduler::scheduleTasks);
+        scheduleThread.start();
+        try {
+            scheduleThread.join(20000);
+            Assertions.assertFalse(scheduleThread.isAlive(), "scheduleTasks method took longer than 20 seconds to complete");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
