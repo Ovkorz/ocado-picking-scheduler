984ca68b81abf7f125815d815a094afe3c79e0da
simon <pupkaszymon@gmail.com>
2023-04-12 22:45:53 +0200

    fix task shifting to before global picking start time

--- src/test/java/scheduler/SchedulerTest.java
+++ src/test/java/scheduler/SchedulerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import testutil.TestCaseLoader;
 import testutil.TestData;
 import testutil.TestingTask;
 
@@ -83,5 +84,32 @@ class SchedulerTest {
             e.printStackTrace();
         }
 
+    }
+
+    @Test
+    void testScheduleTasksCompleteBy() throws IOException {
+        String base_path = TEST_CASES_DIR + "/complete-by";
+        String expected_schedules_path = base_path + "/output.txt";
+        List<List<TestingTask>> expected_schedules = TestCaseLoader.loadSchedules(expected_schedules_path);
+
+        JsonFileLoader loader = new JsonFileLoader();
+
+        String orders_path = base_path + "/orders.json";
+        List<Order> orders = loader.loadOrders(Paths.get(orders_path));
+
+        String store_path = base_path + "/store.json";
+        Store store = loader.loadStore(Paths.get(store_path));
+
+        Scheduler scheduler = new Scheduler(
+                store.getPickers(),
+                store.getPickingStartTime(),
+                store.getPickingEndTime(),
+                orders
+        );
+        List<Task> calculated_schedule = scheduler.scheduleTasks();
+
+        boolean any_match = expected_schedules.stream()
+                .anyMatch(schedule -> TestingTask.compareList(schedule, calculated_schedule));
+        Assertions.assertTrue(any_match);
     }
 }
